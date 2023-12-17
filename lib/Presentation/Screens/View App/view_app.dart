675cd013278772e2f7f83436400ad47054f594fd
Rana76-u <rafiqulislamrana76@gmail.com>
2023-12-18 03:53:33 +0600

    3rd Commit & Push Alhamdulillah

--- lib/Presentation/Screens/View App/view_app.dart
+++ lib/Presentation/Screens/View App/view_app.dart
@@ -1,9 +1,14 @@
+import 'dart:io';
+
+import 'package:appgallery/Presentation/Widgets/image_viewer_gallery.dart';
 import 'package:appgallery/ViewApp%20Bloc/viewapp_bloc.dart';
 import 'package:appgallery/ViewApp%20Bloc/viewapp_events.dart';
 import 'package:appgallery/ViewApp%20Bloc/viewapp_states.dart';
 import 'package:device_apps/device_apps.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_downloader/flutter_downloader.dart';
+import 'package:open_file/open_file.dart';
 
 // ignore: must_be_immutable
 class ViewApp extends StatefulWidget {
@@ -58,6 +63,57 @@ class _ViewAppState extends State<ViewApp> {
     }
   }
 
+  void open(BuildContext context, final int index) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => GalleryPhotoViewWrapper(
+          galleryItems: widget.screenshots,
+          backgroundDecoration: const BoxDecoration(
+            color: Colors.black,
+          ),
+          initialIndex: index,
+          scrollDirection: Axis.horizontal,
+        ),
+      ),
+    );
+  }
+
+  Future<void> _downloadAndOpenFile(BuildContext context) async {
+    // Initialize the downloader
+    //await FlutterDownloader.initialize();
+
+    print(widget.fileLink);
+
+    final taskId = await FlutterDownloader.enqueue(
+      url: widget.fileLink,
+      savedDir: '/storage/emulated/0/Download/', // Change this to the desired directory
+      saveInPublicStorage: true,
+      showNotification: true,
+      openFileFromNotification: true,
+    );
+
+    FlutterDownloader.registerCallback((id, status, progress) {
+      if (id == taskId && status == DownloadTaskStatus.complete) {
+        // The download is complete, open the file
+        _openDownloadedFile(context);
+      }
+    });
+  }
+
+
+  Future<void> _openDownloadedFile(BuildContext context) async {
+    // Replace 'your_directory' with the same directory used for downloading
+    final filePath = '/storage/emulated/0/Download/${widget.name}.apk';
+
+    if (await File(filePath).exists()) {
+      OpenFile.open(filePath);
+    } else {
+      // Handle the case where the file does not exist
+      print('File not found');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return BlocConsumer<ViewAppBloc, ViewAppState>(
@@ -68,7 +124,7 @@ class _ViewAppState extends State<ViewApp> {
         return Scaffold(
           appBar: AppBar(
             leading: IconButton(
-              icon: const Icon(Icons.arrow_back, color: Colors.black),
+              icon: const Icon(Icons.arrow_back,),
               onPressed: () => Navigator.of(context).pop(),
             ),
           ),
@@ -76,104 +132,13 @@ class _ViewAppState extends State<ViewApp> {
             child: Padding(
               padding: const EdgeInsets.symmetric(horizontal: 20),
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
 
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      //icon - image
-                      SizedBox(
-                        height: 80,
-                        width: 80,
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(15),
-                          child: Image.network(
-                            widget.icon,
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      ),
-
-                      //Texts
-                      Padding(
-                        padding: const EdgeInsets.only(left: 15),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              widget.name,
-                              style: const TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 20
-                              ),
-                            ),
-                            /*SizedBox(
-                          width: MediaQuery.of(context).size.width*0.5 - 55,
-                          child: Text(
-                            about,
-                            maxLines: null,
-                            style: const TextStyle(
-                                fontSize: 11,
-                                overflow: TextOverflow.ellipsis,
-                            ),
-                          ),
-                        ),*/
-                            const SizedBox(height: 10,),
-                            Row(
-                              children: [
-                                Column(
-                                  children: [
-                                    const Icon(
-                                      Icons.download_rounded,
-                                      size: 19,
-                                    ),
-                                    Text(
-                                      widget.size,
-                                      style: const TextStyle(
-                                          fontSize: 10
-                                      ),
-                                    ),
-                                  ],
-                                ),
-
-                                const Padding(
-                                  padding: EdgeInsets.symmetric(horizontal: 20),
-                                  child: Text(
-                                    '|',
-                                    style: TextStyle(
-                                        fontSize: 30
-                                    ),
-                                  ),
-                                ),
-
-                                Column(
-                                  children: [
-                                    Text(
-                                      widget.downloadCount.toString(),
-                                      style: const TextStyle(
-                                          fontSize: 14,
-                                          fontWeight: FontWeight.bold
-                                      ),
-                                    ),
-                                    const Text(
-                                      'Downloads',
-                                      style: TextStyle(
-                                          fontSize: 10
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ],
-                            )
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
+                  topAppInfo(),
 
                   //space
-                  const SizedBox(height: 20,),
+                  space(20),
 
                   //open btn
                   state.isUpdateAvailable
@@ -183,6 +148,14 @@ class _ViewAppState extends State<ViewApp> {
                   buttons('open')
                       :
                   buttons('download'),
+
+                  space(25),
+
+                  screenShotsWidget(),
+
+                  space(25),
+
+                  aboutThisApp(),
                 ],
               ),
             ),
@@ -240,6 +213,9 @@ class _ViewAppState extends State<ViewApp> {
               child: Center(
                 child: action == 'update' ?
                 GestureDetector(
+                  onTap: () {
+                    _downloadAndOpenFile(context);
+                  },
                   child: const Text(
                     'Update',
                     style: TextStyle(color: Colors.black),
@@ -271,4 +247,162 @@ class _ViewAppState extends State<ViewApp> {
     );
   }
 
+  Widget space(double size) {
+    return SizedBox(height: size,);
+  }
+
+  Widget topAppInfo() {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        //icon - image
+        SizedBox(
+          height: 80,
+          width: 80,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(15),
+            child: Image.network(
+              widget.icon,
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+
+        //Texts
+        Padding(
+          padding: const EdgeInsets.only(left: 15),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                widget.name,
+                style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20
+                ),
+              ),
+              /*SizedBox(
+                          width: MediaQuery.of(context).size.width*0.5 - 55,
+                          child: Text(
+                            about,
+                            maxLines: null,
+                            style: const TextStyle(
+                                fontSize: 11,
+                                overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                        ),*/
+              const SizedBox(height: 10,),
+              Row(
+                children: [
+                  Column(
+                    children: [
+                      const Icon(
+                        Icons.download_rounded,
+                        size: 19,
+                      ),
+                      Text(
+                        widget.size,
+                        style: const TextStyle(
+                            fontSize: 10
+                        ),
+                      ),
+                    ],
+                  ),
+
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20),
+                    child: Text(
+                      '|',
+                      style: TextStyle(
+                          fontSize: 30
+                      ),
+                    ),
+                  ),
+
+                  Column(
+                    children: [
+                      Text(
+                        widget.downloadCount.toString(),
+                        style: const TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold
+                        ),
+                      ),
+                      const Text(
+                        'Downloads',
+                        style: TextStyle(
+                            fontSize: 10
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget screenShotsWidget() {
+    return SizedBox(
+      height: 170,
+      child: ListView.builder(
+        shrinkWrap: true,
+        scrollDirection: Axis.horizontal,
+        itemCount: widget.screenshots.length,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.only(right: 10),
+            child: SizedBox(
+              height: 170,
+              width: 95,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(8),
+                child: GestureDetector(
+                  onTap: () {
+                    /*Navigator.of(context).push(
+                      MaterialPageRoute(builder: (context) => ImageViewer(imageUrl: widget.screenshots[index],),)
+                    );*/
+                    open(context, index);
+                  },
+                  child: Image.network(
+                    widget.screenshots[index],
+                    fit: BoxFit.contain,
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget aboutThisApp() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text(
+            'About this app',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 14
+          ),
+        ),
+
+        space(10),
+
+        Text(
+          widget.about,
+          style: const TextStyle(
+              fontSize: 12
+          ),
+        ),
+      ],
+    );
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,16 @@ import 'package:appgallery/ViewApp%20Bloc/viewapp_bloc.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_downloader/flutter_downloader.dart';
 import 'Presentation/Widgets/navigation_bar.dart';
 import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await FlutterDownloader.initialize(
+      debug: true, // optional: set to false to disable printing logs to console (default: true)
+      ignoreSsl: true // option: set to false to disable working with http links (default: false)
+  );
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
